e39ed2dff6890645b49390810abadb9a114b6718
SinkingMeteor <badcode@internet.ru>
2024-03-11 20:29:56 +0200

    Engine and framework targets

---
+++ Engine/Tests/Source/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Engine tests" << std::endl;
+    return 0;
+}
---
+++ Framework/Tests/Source/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Framework tests" << std::endl;
+    return 0;
+}
