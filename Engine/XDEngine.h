fc6641481522ab59368a95742c8d01319e113fae
SinkingMeteor <badcode@internet.ru>
2024-04-03 19:37:03 +0300

    xd asserts

--- Engine/XDEngine.h
+++ Engine/XDEngine.h
@@ -7,5 +7,15 @@
 #endif
 
 #ifdef XD_BUILD_TYPE_DEBUG
-    #define XD_DEBUG
+    #define dXD_DEBUG
+#endif
+
+#ifdef dXD_DEBUG
+    #define mXD_ASSERTM(cond, msg)  if (!(cond)) { std::cout << ">> [Assert fail] [ " << __FILE__ << " | " << __func__ << " | " << __LINE__ << " ] : " msg << std::endl; __debugbreak(); }
+    #define mXD_ASSERT(cond)  if (!(cond)) mXD_ASSERTM(cond, "");
+    #define mXD_NOT_IMPLEMENTED() mXD_ASSERTM(false, "Not implemented function reached");
+#else
+    #define mXD_ASSERT(x, msg);
+    #define mXD_ASSERT_S(x);
+    #define mXD_NOT_IMPLEMENTED();
 #endif
--- Framework/Source/FrameworkDummy.cpp
+++ Framework/Source/FrameworkDummy.cpp
@@ -1,11 +1,9 @@
 #include "FrameworkDummy.h"
-#include "Core/EngineDummy.h"
 
 namespace XD
 {
     void FrameworkDummy::SayHello()
     {
-        XD::EngineDummy::SayHello();
     }
 }
 
