6b9497fc9812a1767b07044416888f154cb36dea
SinkingMeteor <badcode@internet.ru>
2024-04-21 19:15:43 +0300

    Trying to load opengl with no glfw

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,13 +1,37 @@
 #include "Main.h"
 #include "Application/Platform/XDApplication_PlatformSelector.h"
 #include "Common/Platform/XDCommon_PlatformSelector.h"
-#include "Common/XD_Utility.h"
 #include "glad.h"
+
+//delete me
+void* NullLoaderProcPtr(const char *name)
+{
+    return nullptr;
+}
 
 int
 main(int argc, char** argv)
 {
-    if(!gladLoadGLLoader()
+
+    //delete me as soon as possible
+    if(false)
+    {
+        mLOG("Start");
+        if (!gladLoadGLLoader((GLADloadproc)NullLoaderProcPtr))
+        {
+            mLOG("Fail");
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+        }
+
+        typedef HGLRC (tWGLCreateContextProcPtr)(HDC);
+
+        XD::XD_Library lib{};
+        X_Call(lib.fLoadLibraryX("opengl32.dll"), "");
+
+        tWGLCreateContextProcPtr* f = lib.fGetProcAddress("wglCreateContext");
+        mLOG(f);
+    }
 
     XD::XD_ApplicationConfig applicationConfig{};
     applicationConfig.m_displayName = "XD";
